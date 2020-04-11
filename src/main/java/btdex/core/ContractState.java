35ffaf60572b257cfcb17d1238bec9aa5a0f9107
shefass <shefass@gmail.com>
2020-04-11 14:14:19 +0300

    moved getters to bottom of class

--- src/main/java/btdex/core/ContractState.java
+++ src/main/java/btdex/core/ContractState.java
@@ -58,66 +58,10 @@ public class ContractState {
 		this.type = type;
 	}
 	
-	public Type getType() {
-		return type;
-	}
-	
-	public long getMarket() {
-		return market;
-	}
-	
-	public Account getMarketAccount() {
-		return marketAccount;
-	}
-	
-	public BurstAddress getAddress() {
-		return address;
-	}
-	
-	public BurstAddress getCreator() {
-		return at.getCreator();
-	}
-
-	public BurstValue getBalance() {
-		return balance;
-	}
-
-	public long getMediator1() {
-		return mediator1;
-	}
-
-	public long getMediator2() {
-		return mediator2;
-	}
-
-	public long getOfferType() {
-		return offerType;
-	}
-
-	public long getState() {
-		return state;
-	}
-	
 	public boolean hasStateFlag(long flag) {
 		return (state & flag) == flag;
 	}
 
-	public long getRate() {
-		return rate;
-	}
-
-	public long getAmountNQT() {
-		return amount;
-	}
-	
-	public long getFeeContract() {
-		return feeContract;
-	}
-	
-	public long getActivationFee() {
-		return at.getMinimumActivation().longValue();
-	}
-	
 	public long getNewOfferFee() {
 		if(type == Type.SELL) {
 			return at.isFrozen() ?
@@ -131,74 +75,62 @@ public class ContractState {
 
 		return getActivationFee();
 	}
-	
-	public boolean hasPending() {
-		return hasPending;
-	}
-	
+
 	public String getAmount() {
 		double dvalue = (double)amount / Contract.ONE_BURST;
 		return NumberFormatting.BURST.format(dvalue);
 	}
 
-	public long getSecurityNQT() {
-		return security;
-	}
-	
-	public long getTaker() {
-		return taker;
-	}
-	
 	public String getSecurity() {
 		double dvalue = (double)security / Contract.ONE_BURST;
 		return NumberFormatting.BURST.format(dvalue);
 	}
-	
+
 	public Method getMethod(String method) {
 		return compiler.getMethod(method);
 	}
 
 	/**
 	 * Add all contracts with matching machine code to the given map.
-	 * 
+	 *
 	 * First call should be with an empty map and then the map can be reused to
 	 * add possibly recently registered contracts.
-	 * 
+	 *
 	 * @param map
-	 * 
+	 *
 	 * @return the most recent ID visited
 	 */
 	public static BurstID addContracts(HashMap<BurstAddress, ContractState> map, BurstID mostRecent){
 		Globals g = Globals.getInstance();
 		BT.setNodeInstance(g.getNS());
-		
+
 		BurstAddress[] atIDs = g.getNS().getAtIds().blockingGet();
-		
+
 		BurstID first = null;
 		BurstID idLimit = BurstID.fromLong(g.isTestnet() ?
 				"9601465021860021685" : "17916279999448178140");
-		
+
 		// reverse order to get the more recent ones first
 		for (int i = atIDs.length-1; i >= 0; i--) {
 			BurstAddress ad = atIDs[i];
-			
+
 			if(first == null)
 				first = ad.getBurstID();
-			
+
 			// avoid running all IDs, we know these past one are useless
 			if(ad.getBurstID().getSignedLongId() == idLimit.getSignedLongId())
 				break;
 			// already visited, no need to continue
 			if(mostRecent!=null && ad.getBurstID().getSignedLongId() == mostRecent.getSignedLongId())
 				break;
-			
+
 			// If the map already have this one stop, since they come in order
 			if(map.containsKey(ad))
 				break;
-			
+
 			AT at = g.getNS().getAt(ad).blockingGet();
 			Type type = Type.INVALID;
-			
+
 			// check the code (should match perfectly)
 			if(Contracts.checkContractCode(at, Contracts.getCodeSell()))
 				type = Type.SELL;
@@ -206,17 +138,17 @@ public class ContractState {
 				type = Type.BUY;
 			else if(Contracts.checkContractCode(at, Contracts.getCodeNoDeposit()))
 				type = Type.NO_DEPOSIT;
-			
+
 			if(type!=Type.INVALID) {
 				ContractState s = new ContractState(type);
 				s.at = at;
-				
+
 				// Check some immutable variables
 				s.compiler = Contracts.getCompiler(type);
 				s.mediator1 = s.getContractFieldValue("mediator1");
 				s.mediator2 = s.getContractFieldValue("mediator2");
 				s.feeContract = s.getContractFieldValue("feeContract");
-				
+
 				// Check if the immutable variables are valid
 				if(g.getMediators().areMediatorsAccepted(s)
 						&& Constants.FEE_CONTRACT == s.getFeeContract()){
@@ -225,14 +157,14 @@ public class ContractState {
 				}
 			}
 		}
-				
+
 		return first;
-	}	
-	
+	}
+
 	public void update(Transaction[] utxs) {
 		updateState(null, utxs);
 	}
-	
+
     private long getContractFieldValue(String field) {
     	int address = compiler.getFieldAddress(field);
         byte[] data = at.getMachineData();
@@ -241,20 +173,20 @@ public class ContractState {
 
         return b.getLong(address * 8);
     }
-    
+
 	private void updateState(AT at, Transaction[] utxs) {
 		Globals g = Globals.getInstance();
-		
+
 		if(at == null)
 			at = g.getNS().getAt(address).blockingGet();
-		
+
 		this.at = at;
 		this.address = at.getId();
 		this.balance = at.getBalance();
-		
+
 		if(at.isDead())
 			type = Type.INVALID;
-		
+
 		// update variables that can change over time
 		if(type == Type.SELL || type == Type.BUY) {
 			this.state = getContractFieldValue("state");
@@ -266,13 +198,13 @@ public class ContractState {
 			this.state = getContractFieldValue("state");
 			this.lockMinutes = getContractFieldValue("lockMinutes");
 		}
-		
+
 		// check rate, type, etc. from transaction history
 		Transaction[] txs = g.getNS().getAccountTransactions(this.address).blockingGet();
 		takeBlock = findTakeBlock(txs);
 		hasPending = processTransactions(txs, takeBlock) || processTransactions(utxs, takeBlock);
 	}
-	
+
 	private int findTakeBlock(Transaction[] txs) {
 		int takeBlock = 0;
 		if(this.state > SellContract.STATE_TAKEN) {
@@ -280,7 +212,7 @@ public class ContractState {
 			for(Transaction tx : txs) {
 				if(takeBlock > 0 && tx.getBlockHeight() < takeBlock)
 					break;
-				
+
 				if(tx.getSender().getSignedLongId() == this.taker &&
 						tx.getAppendages()!=null && tx.getAppendages().length==1 &&
 						tx.getAppendages()[0] instanceof PlaintextMessageAppendix) {
@@ -290,7 +222,7 @@ public class ContractState {
 						takeBlock = tx.getBlockHeight();
 					}
 				}
-				
+
 				// we also look for the address definition of a taken buy order (should be more recent than the takeBlock
 				if(type == Type.BUY && tx.getSender().getSignedLongId() == this.taker
 						&& tx.getType() == 1 /* TYPE_MESSAGING */
@@ -325,22 +257,22 @@ public class ContractState {
 		}
 		return takeBlock;
 	}
-	
+
 	private boolean processTransactions(Transaction[] txs, int blockHeightLimit) {
 		if(txs == null)
 			return false;
-		
+
 		Globals g = Globals.getInstance();
 		boolean hasPending = false;
 
 		for(Transaction tx : txs) {
 			if(tx.getId().getSignedLongId() == lastTxId)
 				break;
-			
+
 			// Only transactions for this contract
 			if(tx.getRecipient()==null || !tx.getRecipient().equals(getAddress()))
 				continue;
-			
+
 			// We only accept configurations with 2 confirmations or more
 			// but also get pending info from the user
 			if(tx.getConfirmations() < Constants.PRICE_NCONF) {
@@ -351,17 +283,17 @@ public class ContractState {
 				else
 					continue;
 			}
-			
+
 			// order configurations should be set by the creator (or the taker of a buy is sending his address)
 			if(!tx.getSender().equals(at.getCreator()))
 				continue;
-			
+
 			if(tx.getRecipient().equals(address)
 					&& (tx.getSender().equals(at.getCreator()) || (tx.getSender().getSignedLongId() == taker))
 					&& tx.getType() == 1 /* TYPE_MESSAGING */
 					&& tx.getSubtype() == 0 /* SUBTYPE_MESSAGING_ARBITRARY_MESSAGE */
 					&& tx.getAppendages()!=null && tx.getAppendages().length > 0) {
-				
+
 				TransactionAppendix append = tx.getAppendages()[0];
 				if(append instanceof PlaintextMessageAppendix) {
 					PlaintextMessageAppendix appendMessage = (PlaintextMessageAppendix) append;
@@ -381,7 +313,7 @@ public class ContractState {
 							accountFields = accountJson.getAsString();
 						if(rateJson!=null)
 							rate = Long.parseLong(rateJson.getAsString());
-						
+
 						// parse the account fields (buy do not have it)
 						if(accountFields != null) {
 							for(Market m : Markets.getMarkets()) {
@@ -391,11 +323,11 @@ public class ContractState {
 								}
 							}
 						}
-						
+
 						// set this as the accepted last TxId
 						if(tx.getConfirmations() >= Constants.PRICE_NCONF) {
 							lastTxId = tx.getId().getSignedLongId();
-						
+
 							// done, only the more recent (2 confirmations) matters
 							break;
 						}
@@ -407,5 +339,73 @@ public class ContractState {
 			}
 		}
 		return hasPending;
+	}
+
+	public boolean hasPending() {
+		return hasPending;
+	}
+
+	public long getSecurityNQT() {
+		return security;
+	}
+
+	public long getTaker() {
+		return taker;
+	}
+
+	public long getRate() {
+		return rate;
+	}
+
+	public long getAmountNQT() {
+		return amount;
+	}
+
+	public long getFeeContract() {
+		return feeContract;
+	}
+
+	public long getActivationFee() {
+		return at.getMinimumActivation().longValue();
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public long getMarket() {
+		return market;
+	}
+
+	public Account getMarketAccount() {
+		return marketAccount;
+	}
+
+	public BurstAddress getAddress() {
+		return address;
+	}
+
+	public BurstAddress getCreator() {
+		return at.getCreator();
+	}
+
+	public BurstValue getBalance() {
+		return balance;
+	}
+
+	public long getMediator1() {
+		return mediator1;
+	}
+
+	public long getMediator2() {
+		return mediator2;
+	}
+
+	public long getOfferType() {
+		return offerType;
+	}
+
+	public long getState() {
+		return state;
 	}
 }
