685d33cadac16db1c466cfb6a841abbb4390bcc6
jjos2372 <jjos2372@gmail.com>
2020-07-05 11:15:42 -0300

    Another mediator

--- src/main/java/btdex/core/Constants.java
+++ src/main/java/btdex/core/Constants.java
@@ -35,7 +35,7 @@ public class Constants {
     public static final String DEF_CONF_FILE = "config.properties";
 
     public static final String[] MEDIATORS = {"TLYF-7EBX-FBLY-DFX86", "P3D9-QX3S-7YHZ-BYLZD"};
-    public static final String[] MEDIATORS_TESTNET = {"6ET8-WUKM-3HS8-CN4KM", "D3S9-8L56-UMLL-6EDFX"};
+    public static final String[] MEDIATORS_TESTNET = {"6ET8-WUKM-3HS8-CN4KM", "D3S9-8L56-UMLL-6EDFX", "E9UA-FX37-CHPE-568RD"};
 
     // FIXME: set the fee contract
     public static final long FEE_CONTRACT = 222222L;
