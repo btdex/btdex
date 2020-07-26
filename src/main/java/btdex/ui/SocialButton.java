d38ec2dcb9d37f7c96d4237802255e51b204f6d8
jjos2372 <jjos2372@gmail.com>
2020-07-26 10:17:13 -0300

    Hashtags

--- src/main/java/btdex/ui/SocialButton.java
+++ src/main/java/btdex/ui/SocialButton.java
@@ -15,7 +15,6 @@ import btdex.core.*;
 import btdex.locale.Translation;
 import burst.kit.entity.response.AssetOrder;
 import jiconfont.IconCode;
-import jiconfont.icons.font_awesome.FontAwesomeBrands;
 import jiconfont.swing.IconFontSwing;
 
 public class SocialButton extends JButton implements ActionListener {
@@ -71,7 +70,7 @@ public class SocialButton extends JButton implements ActionListener {
 		default:
 			icon = Icons.TWITTER;
 			name = "Twitter";
-			tags = "\n#DEX #crypto @btdex_trade";
+			tags = "\n#DEX #crypto #NoKYCOnly #KYClite @btdex_trade";
 			break;
 		}
 		setIcon(IconFontSwing.buildIcon(icon, size, color));
