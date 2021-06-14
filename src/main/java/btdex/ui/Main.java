76bcbc6a519517f7ee9a4f543abec1826c782d65
jjos2372 <jjos2372@gmail.com>
2021-06-14 11:05:27 -0300

    More on new ticker.

--- src/main/java/btdex/ui/Main.java
+++ src/main/java/btdex/ui/Main.java
@@ -236,7 +236,7 @@ public class Main extends JFrame implements ActionListener {
 		copyAddButton.getMainButton().setFont(largeFont);
 
 		sendButton = new JButton(i.get(Icons.SEND));
-		sendButton.setToolTipText(tr("main_send", "BURST"));
+		sendButton.setToolTipText(tr("main_send", Constants.BURST_TICKER));
 		sendButton.addActionListener(this);
 
 		content.add(tabbedPane, BorderLayout.CENTER);
@@ -263,7 +263,7 @@ public class Main extends JFrame implements ActionListener {
 		balanceLabel.setFont(largeFont);
 		lockedBalanceLabel = new JLabel(tr("main_plus_locked", NumberFormatting.BURST.format(0)));
 		lockedBalanceLabel.setToolTipText(tr("main_amount_locked"));
-		top.add(new Desc(tr("main_balance", "BURST"), balanceLabel, lockedBalanceLabel));
+		top.add(new Desc(tr("main_balance", Constants.BURST_TICKER), balanceLabel, lockedBalanceLabel));
 		top.add(new Desc("  ", sendButton));
 
 		topRight.add(new Desc("  ", createSettingsButton(largeFont)));
