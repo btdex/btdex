d7fa3d984788a9da945b1052588ba6fa0930e347
Jegor <aaq7for@gmail.com>
2022-01-11 22:47:20 +0300

    validate only P2SH addresses

--- src/main/java/btdex/markets/MarketBHD.java
+++ src/main/java/btdex/markets/MarketBHD.java
@@ -2,15 +2,26 @@ package btdex.markets;
 
 import java.util.HashMap;
 
-import btdex.core.Globals;
 import btdex.locale.Translation;
 
 public class MarketBHD extends MarketCrypto {
 	
+	public static final String TICKER = "BHD";
+
 	public String getTicker() {
-		return "BHD";
+		return TICKER;
 	}
-	
+
+	@Override
+	public String getChainDetails() {
+		return "BitcoinHD native chain";
+	}
+
+	@Override
+	public String getExplorerLink() {
+		return "https://www.btchd.org/explorer/";
+	}
+
 	@Override
 	public long getID() {
 		return MARKET_BHD;
@@ -26,16 +37,8 @@ public class MarketBHD extends MarketCrypto {
 		super.validate(fields);
 		
 		String addr = fields.get(ADDRESS);
-		
-		if(addr.startsWith(Globals.getInstance().isTestnet() ? "thd1" : "bhd1")) {
-			try {
-				Bech32.decode(addr);
-			}
-			catch (Exception e) {
-				throw new Exception(Translation.tr("mkt_invalid_address", addr, toString()));
-			}
-		}
-		else if(!BTCAddrValidator.validate(addr)) {
+
+		if( !(addr.startsWith("3") && BTCAddrValidator.validate(addr)) ) {
 			throw new Exception(Translation.tr("mkt_invalid_address", addr, toString()));
 		}
 	}
